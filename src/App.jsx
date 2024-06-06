728c32125ae780dd62b16ee29534b7d86c616eb6
Arce-Mdina <nets_05basins@icloud.com>
2024-06-06 18:00:43 +0100

    Edited landing page

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import './App.css'
+
+import gsap from 'gsap'
 
 function App() {
 
+  useEffect(() => {
+    gsap.from('.heading', { opacity: 0, duration: 1, y: -100, delay: 1 })
+    gsap.from('.desc-heading', { opacity: 0, duration: 1, y: -100 })
+  }, [])
+  
+
   return (
     <>
-      <h1>Covid-19</h1>
+      <h1 className="heading">Covid-19</h1>
+      <hr style={{ height: "3px", background: "#1daa34", border: "none" }}></hr>
+      <h2 className="desc-heading">A dive into the pandemic and our past.</h2>
     </>
   )
 }
