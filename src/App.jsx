84ffdae4f23844690ae009f8a32355ecbbc3b9e7
Arce-Mdina <nets_05basins@icloud.com>
2024-06-07 07:38:38 +0100

    Added about section

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,17 @@ import './App.css'
 
 import gsap from 'gsap'
 
+import About from './sections/About';
+import Cases from './sections/Cases';
+import Lockdown from './sections/Lockdown';
+import Pandemic from './sections/Pandemic';
+import Preventions from './sections/Preventions';
+import Quarantine from './sections/Quarantine';
+import Science from './sections/Science';
+import Symptoms from './sections/Symptoms';
+import Treatments from './sections/Treatments';
+import Virus from './sections/Virus';
+
 function App() {
 
   useEffect(() => {
@@ -13,9 +24,17 @@ function App() {
 
   return (
     <>
-      <h1 className="heading">Covid-19</h1>
-      <hr style={{ height: "2.7px", background: "#1daa34", border: "none" }}></hr>
-      <h2 className="desc-heading">A dive into the pandemic and our past.</h2>
+      <div className="headings">
+        <div className="header">
+          <h1 className="heading">Covid-19</h1>
+          <hr style={{ height: "2.7px", background: "#1daa34", border: "none" }}></hr>
+          <h2 className="desc-heading">A dive into the pandemic and our past.</h2>
+        </div>
+      </div>
+
+      <div className="content">
+        <About />
+      </div>
     </>
   )
 }
--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>About</div>
+    <>
+      <div className="desc">What is Covid-19?</div>
+    </>
   )
 }
 
