e529be73bf1d0dc032e7e97e9585da9defc76a7a
Arce-Mdina <nets_05basins@icloud.com>
2024-06-08 08:09:45 +0100

    Justify text

--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -7,7 +7,7 @@ const About = () => {
       <div className="desc">
         <h2 style={{ fontSize: "2.3em" }}>What is Covid-19?</h2>
       </div>
-      <div>
+      <div className="justify">
         <p>Covid-19, also known as the Coronovirus, is a</p>
       </div>
     </>
