31ee786e7cc84ea0686571462ed1fe9a494960c6
Arce-Mdina <nets_05basins@icloud.com>
2024-06-08 15:43:54 +0100

    Added routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import React, { useEffect } from 'react'
 import './App.css'
 
+import { BrowserRouter, Route, Routes, Link } from 'react-router-dom';
+
 import gsap from 'gsap'
 
 import About from './sections/About';
@@ -33,25 +35,21 @@ function App() {
       </div>
 
       <div className="content">
-        <About />
 
-        <Science />
-
-        <Symptoms />
-
-        <Treatments />
-
-        <Preventions />
-
-        <Pandemic />
-
-        <Quarantine />
-
-        <Lockdown />
-
-        <Impacts />
-
-        <Cases />
+        <BrowserRouter basename='/covid-19'>
+          <Routes>
+            <Route path="/" element={<About />}/>
+            <Route path="/science-behind-covid" element={<Science />}/>
+            <Route path="/symptoms" element={<Symptoms />}/>
+            <Route path="/treatments" element={<Treatments />}/>
+            <Route path="/preventions" element={<Preventions />}/>
+            <Route path="/pandemic" element={<Pandemic />}/>
+            <Route path="/quarantine" element={<Quarantine />}/>
+            <Route path="/lockdown" element={<Lockdown />}/>
+            <Route path="/impacts" element={<Impacts />}/>
+            <Route path="/stats" element={<Cases />}/>
+          </Routes>
+        </BrowserRouter>
       </div>
     </>
   )
